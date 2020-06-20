0858dfeef72c8638fc16224b7bb69b6973627639
Shamik H <shamik05@hotmail.com>
2020-06-20 17:53:30 -0400

    Array to generate timeblocks; added current time

--- Assets/script.js
+++ Assets/script.js
@@ -1,2 +1,18 @@
+$(document).ready(function () {
+  $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
 
+  var workTimes = [9, 10, 11, 12, 1, 2, 3, 4, 5];
+  console.log(workTimes);
 
+  $.each(workTimes, function (index, value) {
+    var timeRow = $("<div class='row time-block'>");
+    var timeCol0 = $("<div class='col hour'>");
+    timeCol0.text(value);
+    var timeCol1 = $("<textarea class='col-8 description past'>");
+    var timeCol2 = $("<button class='col-2 saveBtn' style='font-size: 2.5rem'>");
+    var saveFA = $("<i class='far fa-save'>")
+    $(timeCol2).append(saveFA);
+    $(timeRow).append(timeCol0, timeCol1, timeCol2);
+    $(".container-fluid").append(timeRow);
+  });
+});

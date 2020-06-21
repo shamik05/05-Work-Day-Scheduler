3b5d2190a68c69e8b048976561e1dbdb9c698fac
Shamik H <shamik05@hotmail.com>
2020-06-21 16:31:48 -0400

    Added color coding to timeblocks based on tenses

--- Assets/script.js
+++ Assets/script.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-  $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss A"));
+  $("#currentDay").text(moment().format("dddd, MMMM Do YYYY, h:mm:ss A"));
   
   var workTimes = [moment().hours(9).minutes(0).seconds(0)];
   for(let i=0; i<=22; i++){
@@ -14,11 +14,18 @@ $(document).ready(function(){
     var timeCol0 = $("<div class='col-2 hour'>");
     $(timeCol0).text(workTimes[index].format("h A"));
     var timeCol1 = $("<textarea class='col-8 description' placeholder='Type your activities for the hour...'>");
-    // $(timeCol1).attr("id", "text"+workTimes[index]._d.getHours());
     var timeCol2 = $("<button class='col-2 saveBtn' style='font-size: 2.5rem'>");
     $(timeCol2).attr("id", "save"+workTimes[index]._d.getHours());
     $(timeCol1).val(timeGetText(workTimes[index]._d.getHours()));
     var saveFA = $("<i class='far fa-save'>");
+    var timeCur = moment();
+    if(moment(workTimes[index]).isBefore(timeCur) && workTimes[index]._d.getHours()!=timeCur._d.getHours()){
+      $(timeCol1).addClass("past");console.log("past");
+    }else if(moment(workTimes[index]).isAfter(timeCur)){
+      $(timeCol1.addClass("future"));console.log("future");
+    } else {
+      $(timeCol1.addClass("present"));console.log("present");
+    }
     $(timeCol2).append(saveFA);
     $(timeRow).append(timeCol0, timeCol1, timeCol2);
     $(".container-fluid").append(timeRow);
@@ -27,13 +34,13 @@ $(document).ready(function(){
   $("button").click(function(){
     // console.log(this.id);
     let timeText = $(this).prev().val();
-    if(timeText === ""){
-      // alert($(this).prev().prev().text()+" Timeblock has no activities to save");
-      return;
-    }
+    // if(timeText === ""){
+    //   alert($(this).prev().prev().text()+" Timeblock has no activities to save");
+    //   return;
+    // }
     let saveBtnEl = this.id;
     timeSaveText(saveBtnEl, timeText);
-    // alert($(this).prev().prev().text()+" Timeblock saved successfully!")
+    alert($(this).prev().prev().text()+" Timeblock saved successfully!")
   });
 
   function timeSaveText(key, value){

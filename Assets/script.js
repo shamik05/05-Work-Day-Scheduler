cfd938c7ba32fa494d7d71f815da9809f656282e
Shamik H <shamik05@hotmail.com>
2020-06-22 01:25:39 -0400

    Added event to populate timeblocks with duration

--- Assets/script.js
+++ Assets/script.js
@@ -1,39 +1,64 @@
 $(document).ready(function(){
   setInterval(function(){
-    $("#currentDay").text(moment().format("dddd, MMMM Do YYYY, h:mm:ss A"));
+    $("#currentDay").text(moment().format("[Today is] MMMM D, h:mm:ss A"));
   },1000);
-  
-  var workTimes = [moment().hours(9).minutes(0).seconds(0)];
-  for(let i=0; i<=22; i++){
-    var timeClone = workTimes[i].clone();
-    timeClone.add(1, 'hours');
-    workTimes[i+1] = timeClone;
-  }
-  // console.log(workTimes[0]._d.getHours());
 
-  $.each(workTimes, function (index) {
-    var timeRow = $("<div class='row time-block'>");
-    var timeCol0 = $("<div class='col-2 hour'>");
-    $(timeCol0).text(workTimes[index].format("h A"));
-    var timeCol1 = $("<textarea class='col-8 description' placeholder='Type your activities for the hour...'>");
-    var timeCol2 = $("<button class='col-2 saveBtn' style='font-size: 2.5rem'>");
-    $(timeCol2).attr("id", "save"+workTimes[index]._d.getHours());
-    $(timeCol1).val(timeGetText(workTimes[index]._d.getHours()));
-    var saveFA = $("<i class='far fa-save'>");
-    var timeCur = moment();
-    if(moment(workTimes[index]).isBefore(timeCur) && workTimes[index]._d.getHours()!=timeCur._d.getHours()){
-      $(timeCol1).addClass("past");console.log("past");
-    }else if(moment(workTimes[index]).isAfter(timeCur)){
-      $(timeCol1.addClass("future"));console.log("future");
-    } else {
-      $(timeCol1.addClass("present"));console.log("present");
-    }
-    $(timeCol2).append(saveFA);
-    $(timeRow).append(timeCol0, timeCol1, timeCol2);
-    $(".container-fluid").append(timeRow);
-  });
+  var timeList = [];
+  for(let i=1; i<=12; i++){
+    timeList.push("<option value="+i+">"+i+"</option>")
+  };
   
-  $("button").click(function(){
+  $("#startTime").append(timeList);
+  $("#endTime").append(timeList);
+  
+  function renderTimeslots(duration){
+    console.log(duration)
+    $(".container-fluid").empty();
+    var workTimes = [moment().hours(9).minutes(0).seconds(0)];
+    for(let i=0; i<=duration; i++){
+      var timeClone = workTimes[i].clone();
+      timeClone.add(1, 'hours');
+      workTimes[i+1] = timeClone;
+    }
+
+    $.each(workTimes, function (index) {
+      var timeRow = $("<div class='row time-block'>");
+      var timeCol0 = $("<div class='col hour'>");
+      $(timeCol0).text(workTimes[index].format("ddd, hA"));
+      var timeCol1 = $("<textarea class='col-8 description' placeholder='Type your activities for the hour...'>");
+      var timeCol2 = $("<button class='col-2 saveBtn' style='font-size: 2.5rem'>");
+      $(timeCol2).attr("id", "save"+workTimes[index]._d.getHours());
+      $(timeCol1).val(timeGetText(workTimes[index]._d.getHours()));
+      var saveFA = $("<i class='far fa-save'>");
+      var timeCur = moment();
+      if(moment(workTimes[index]).isBefore(timeCur) && workTimes[index]._d.getHours()!=timeCur._d.getHours()){
+        $(timeCol1).addClass("past");
+      }else if(moment(workTimes[index]).isAfter(timeCur)){
+        $(timeCol1.addClass("future"));
+      } else {
+        $(timeCol1.addClass("present"));
+      }
+      $(timeCol2).append(saveFA);
+      $(timeRow).append(timeCol0, timeCol1, timeCol2);
+      $(".container-fluid").append(timeRow);
+    });
+  }
+
+  // renderTimeslots();
+
+  $(".timeSetup").click(function(){
+    var start = moment($("#startTime").val()+$("#startAMPM").val(),"hA");
+    var end = moment($("#endTime").val()+$("#endAMPM").val(),"hA");
+    
+    // Copied 
+    if ( (start.hour() >=12 && end.hour() <=12 ) || end.isBefore(start) )
+	  {
+		end.add(1, "days");       // handle spanning days endTime
+    }
+    renderTimeslots(Math.abs(start.diff(end,"hours")));
+  })
+
+  $(".col-2").click(function(){
     // console.log(this.id);
     let timeText = $(this).prev().val();
     // if(timeText === ""){
@@ -55,4 +80,6 @@ $(document).ready(function(){
       return value;
     };
   }
+
+  
 })
